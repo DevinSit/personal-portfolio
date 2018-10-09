935ad59cd3b6211d925351c2876072d474c1b92d
Devin Sit <devin@devinsit.com>
2018-10-09 13:43:41 -0400

    Filled out the content for the Home section; the Tag Line Circle and Next Section Nav parts have been created.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,26 @@
 import {Component} from "preact";
+import NextSection from "assets/NextSection.svg";
 import style from "./Home.scss";
 
 export default class Home extends Component {
 	render() {
 		return (
 			<div className={style.homeContainer}>
-                <div className={style.home}>
+                <TagLineCircle />
+                <div className={style.nextSectionNavContainer}>
+                    <img src={NextSection} className={style.nextSectionNav} />
                 </div>
 			</div>
 		);
 	}
 }
+
+const TagLineCircle = () => (
+    <div className={style.tagLineContainer}>
+        <div className={style.tagLineCircle}>
+            <h3 className={style.subTagLine}>One of a kind</h3>
+            <h1 className={style.tagLine}>Full Spectrum</h1>
+            <h1 className={style.tagLine}>Developer</h1>
+        </div>
+    </div>
+);
