e8ac69a9afc44723a20546b1019bb68e77ce62c5
Devin Sit <devin@devinsit.com>
2018-10-09 16:24:12 -0400

    Disabled adding hashes to css module classes/ids; added click handler for the Header to jump to the About section.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,29 @@
-import Preact from "preact";
+import {Component} from "preact";
 import Logo from "assets/Logo.svg";
 import style from "./Header.scss";
 
-const Header = () => (
-    <header id={style.header}>
-        <img src={Logo} className={style.logo} />
-        <nav className={style.nav}>
-            <a className={style.navItem}>ABOUT</a>
-            <a className={style.navItem}>SKILLS</a>
-            <a className={style.navItem}>RESUME</a>
-            <a className={style.navItem}>CONTACT</a>
-        </nav>
-    </header>
-);
+export default class Header extends Component {
+    onNavClick = (id) => () => {
+        const element = document.getElementById(id);
+        element.scrollIntoView({behavior: "smooth", block: "start", inline: "start"});
+    };
 
-export default Header;
+    render() {
+        return (
+            <header id={style.header}>
+                <img src={Logo} className={style.logo} />
+                <nav className={style.nav}>
+                    <a
+                        className={style.navItem}
+                        onClick={this.onNavClick("aboutContainer")}
+                    >
+                        ABOUT
+                    </a>
+                    <a className={style.navItem}>SKILLS</a>
+                    <a className={style.navItem}>RESUME</a>
+                    <a className={style.navItem}>CONTACT</a>
+                </nav>
+            </header>
+        );
+    }
+};
