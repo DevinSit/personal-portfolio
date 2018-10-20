82c9dd02561019667aeabf0e786349c0b1527330
Devin Sit <devin@devinsit.com>
2018-10-19 22:17:45 -0400

    Starting to flesh out the Skills section.

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -5,8 +5,38 @@ export default class Skills extends Component {
     render() {
         return (
             <div id={style.skillsContainer}>
-                Skills
+                <Header />
+
+                <div className={style.skillsContent}>
+                    <SkillSelector />
+                    <SkillDescription />
+                </div>
             </div>
         );
     }
 }
+
+const Header = () => (
+    <div className={style.header}>
+        <div className={style.headerLeft}>
+            <h1>SKI</h1>
+        </div>
+
+        <div className={style.headerRight}>
+            <h1>LLS</h1>
+        </div>
+    </div>
+);
+
+const SkillSelector = () => (
+    <div className={style.skillSelector}>
+        <p>So what exactly can a Full Spectrum Developer do?</p>
+        <p>Let's take a look.</p>
+    </div>
+);
+
+const SkillDescription = () => (
+    <div className={style.skillDescription}>
+        <h1>Frontend</h1>
+    </div>
+);
