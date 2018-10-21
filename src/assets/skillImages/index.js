30ad1f12df26519ec5399d5759285b55efb3b6f9
Devin Sit <devin@devinsit.com>
2018-10-20 21:36:38 -0400

    Basically got the Skills section looking exactly like how it is in the design.

---
+++ src/assets/skillImages/index.js
@@ -0,0 +1,9 @@
+import ReactLogo from "./ReactLogo.svg";
+import ReduxLogo from "./ReduxLogo.svg";
+import SassLogo from "./SassLogo.svg";
+
+export default {
+    ReactLogo,
+    ReduxLogo,
+    SassLogo
+};
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,11 +1,19 @@
 import {Component} from "preact";
+import classNames from "classnames";
+import SkillImages from "assets/skillImages";
+import Logo from "assets/Logo.svg";
 import style from "./Skills.scss";
+
+const SKILLS = [
+    "FRONTEND", "BACKEND", "MOBILE", "DESIGN", "DEVOPS", "CLOUD", "DATA"
+];
 
 export default class Skills extends Component {
     render() {
         return (
             <div id={style.skillsContainer}>
                 <Header />
+                <ContentHeader />
 
                 <div className={style.skillsContent}>
                     <SkillSelector />
@@ -19,24 +27,64 @@ export default class Skills extends Component {
 const Header = () => (
     <div className={style.header}>
         <div className={style.headerLeft}>
-            <h1>SKI</h1>
+            <h1 className={style.headerText}>SKI</h1>
         </div>
 
         <div className={style.headerRight}>
-            <h1>LLS</h1>
+            <h1 className={style.headerText}>LLS</h1>
+        </div>
+    </div>
+);
+
+const ContentHeader = () => (
+    <div className={classNames(style.header, style.contentHeader)}>
+        <div className={classNames(style.headerLeft, style.contentHeaderLeft)}>
+            <p className={style.skillSelectorDescription}>
+                So what exactly can a <span>Full Spectrum Developer</span> do?
+            </p>
+            <p className={style.skillSelectorDescription}>Let's take a look.</p>
+        </div>
+
+        <div className={classNames(style.headerRight, style.contentHeaderRight)}>
+            <h2 className={style.skillDescriptionHeader}>FRONTEND</h2>
         </div>
     </div>
 );
 
 const SkillSelector = () => (
-    <div className={style.skillSelector}>
-        <p>So what exactly can a Full Spectrum Developer do?</p>
-        <p>Let's take a look.</p>
+    <div className={style.skillSelectorContainer}>
+        <ul className={style.skillSelector}>
+            {SKILLS.map((skill, index) => <SkillSelectorItem text={skill} selected={index === 0}/>)}
+        </ul>
     </div>
 );
 
-const SkillDescription = () => (
-    <div className={style.skillDescription}>
-        <h1>Frontend</h1>
-    </div>
+const SkillSelectorItem = ({text = "", selected = false}) => (
+    <li
+        className={classNames(
+            style.skillSelectorItem,
+            {[style.selectedItem]: selected}
+        )}
+    >
+        <span>{text}</span>
+        {selected && <div className={style.selectedCircle} />}
+    </li>
 );
+
+class SkillDescription extends Component {
+    render() {
+        return (
+            <div className={style.skillDescriptionContainer}>
+                <p className={style.skillDescription}>
+                    Ah, frontend development — my bread and butter; what first got me into web development and what I've gone on to become rather comfortable with. These days, I'm heavily into using <span className={style.highlight}>React</span> and <span className={style.highlight}>Redux</span> with <span className={style.highlight}>Sass</span> to build <span className={style.highlight}>Single Page Applications</span>. Molding Flexbox and React components to my whim, I've come to really enjoy building beautiful, interactive interfaces from scratch.
+                </p>
+
+                <div className={style.skillImages}>
+                    <img src={SkillImages.ReactLogo} className={style.skillImage} />
+                    <img src={SkillImages.ReduxLogo} className={style.skillImage} />
+                    <img src={SkillImages.SassLogo} className={style.skillImage} />
+                </div>
+            </div>
+        );
+    }
+}
