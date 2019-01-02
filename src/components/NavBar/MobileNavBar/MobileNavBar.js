dfa17991b9441a750f4560d8d6861fca8a9d1e52
Devin Sit <devin@devinsit.com>
2019-01-01 22:42:58 -0500

    Added the mobile menu background circle transition.

--- src/components/NavBar/MobileNavBar/MobileNavBar.js
+++ src/components/NavBar/MobileNavBar/MobileNavBar.js
@@ -1,19 +1,29 @@
 import {Component} from "preact";
+import classNames from "classnames";
 import {Logo} from "components/common";
 import {IconMenu} from "images/";
 import {debounceWithLeading, scrollTo} from "utils/helpers";
 import "./MobileNavBar.scss";
 
 export default class MobileNavBar extends Component {
+    state = {
+        menuOpen: false
+    }
+
     onNavClick = (id) => () => scrollTo(id);
+    onMenuClick = () => this.setState({menuOpen: !this.state.menuOpen});
 
     render() {
+        const {menuOpen} = this.state;
+
         return (
             <div className="navbar-mobile">
                 <nav className="navbar-nav">
                 </nav>
 
-                <button className="navbar-open-button">
+                <div className={classNames("circle", {"circle-expand": menuOpen})}></div>
+
+                <button className="navbar-open-button" onClick={this.onMenuClick}>
                     <img src={IconMenu} alt="menu" />
                 </button>
             </div>
