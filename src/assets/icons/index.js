b061917996a07279fff843c9574daa676df95337
Devin Sit <devin@devinsit.com>
2020-02-14 22:15:56 -0500

    Fleshed out the back side of the project cards, with placeholders for information and buttons for linking to github/external site.

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,4 +1,6 @@
 import IconEmail from "./icon-email.svg";
+import IconExternalLink from "./icon-external-link.svg";
+import IconGithub from "./icon-github.svg";
 import IconLinkedin from "./icon-linkedin.svg";
 import IconLogo from "./icon-logo.svg";
 import IconLogoInverted from "./icon-logo-inverted.svg";
@@ -6,6 +8,8 @@ import IconNextSection from "./icon-next-section.svg";
 
 export {
     IconEmail,
+    IconExternalLink,
+    IconGithub,
     IconLinkedin,
     IconLogo,
     IconLogoInverted,
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,25 +1,40 @@
 import {useMemo, useState} from "preact/hooks";
 import classNames from "classnames";
+import {IconExternalLink, IconGithub} from "assets/icons";
 import {SectionHeader} from "components/common";
 import "./Projects.scss";
 
 const PROJECTS_INFO = [
     {
         title: "Transitr",
-        description: "Mobile app for finding OC Transpo bus arrival times using SMS instead of data."
+        description: "Mobile app for finding OC Transpo bus arrival times using SMS instead of data.",
+        reason: "Because I got tired of manually texting the bus service.",
+        techStack: ["TypeScript", "React Native", "Redux + Redux Saga"],
+        githubLink: "https://github.com/DevinSit/transitr",
+        externalLink: "https://github.com/DevinSit/transitr/releases"
     },
     {
         title: "Transitr",
-        description: "Mobile app for finding OC Transpo bus arrival times using SMS instead of data."
+        description: "Mobile app for finding OC Transpo bus arrival times using SMS instead of data.",
+        reason: "Because I got tired of manually texting the bus service.",
+        techStack: ["TypeScript", "React Native", "Redux + Redux Saga"],
+        githubLink: "https://github.com/DevinSit/transitr",
+        externalLink: "https://github.com/DevinSit/transitr/releases"
     },
     {
         title: "Transitr",
-        description: "Mobile app for finding OC Transpo bus arrival times using SMS instead of data."
+        description: "Mobile app for finding OC Transpo bus arrival times using SMS instead of data.",
+        reason: "Because I got tired of manually texting the bus service.",
+        techStack: ["TypeScript", "React Native", "Redux + Redux Saga"],
+        githubLink: "https://github.com/DevinSit/transitr",
+        externalLink: "https://github.com/DevinSit/transitr/releases"
     }
 ];
 
 const Projects = () => {
-    const projectCards = useMemo(() => PROJECTS_INFO.map((project) => <ProjectCard {...project} />));
+    const projectCards = useMemo(() => PROJECTS_INFO.map((project) => (
+        <ProjectCard key={project.title} {...project} />
+    )));
 
     return (
         <div id="projects">
@@ -40,8 +55,40 @@ const Projects = () => {
     );
 };
 
-const ProjectCard = ({title, description}) => {
-    const [isFlipped, setFlipped] = useState(false);
+const FloatingActionLink = ({className, icon, iconAlt = "", link = ""}) => (
+    <a
+        className={classNames(className, "floating-action-link")}
+        href={link}
+        target="_blank"
+        rel="noopener noreferrer"
+        onClick={(e) => e.stopPropagation()}
+    >
+        <img src={icon} alt={iconAlt} />
+    </a>
+);
+
+const TechStackListItem = ({item}) => (
+    <li className="project-card-tech-stack-list-item">
+        {item}
+    </li>
+);
+
+const ProjectCard = ({
+    title = "",
+    description = "",
+    reason = "",
+    techStack = [],
+    githubLink = "",
+    externalLink = ""
+}) => {
+    const [isFlipped, setFlipped] = useState(true);
+
+    const techStackList = useMemo(() => techStack.map((item) => (
+        <TechStackListItem
+            key={item}
+            item={item}
+        />
+    )), [techStack]);
 
     return (
         <div className="project-card" onClick={() => setFlipped(!isFlipped)}>
@@ -56,7 +103,37 @@ const ProjectCard = ({title, description}) => {
                 </div>
 
                 <div className="project-card-back">
-                    <p>TEST</p>
+                    <div className="project-card-back-section">
+                        <h3 className="project-card-back-header">Why?</h3>
+
+                        <ul className="project-card-back-list">
+                            <li>{reason}</li>
+                        </ul>
+                    </div>
+
+                    <div className="project-card-back-section">
+                        <h3 className="project-card-back-header">Tech Stack</h3>
+
+                        <ul className="project-card-back-list project-card-list-tech-stack">
+                            {techStackList}
+                        </ul>
+                    </div>
+
+                    <div className="project-card-back-navigation">
+                        <FloatingActionLink
+                            className="project-card-navigation-button"
+                            icon={IconGithub}
+                            iconAlt="github"
+                            link={githubLink}
+                        />
+
+                        <FloatingActionLink
+                            className="project-card-navigation-button"
+                            icon={IconExternalLink}
+                            iconAlt="external link"
+                            link={externalLink}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
