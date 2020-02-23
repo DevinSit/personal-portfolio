8e81cca77b2739c548cc725c25c9ce5026dc146f
Devin Sit <devin@devinsit.com>
2020-02-22 23:44:48 -0500

    Refactored the DesktopNavBar to use functional components + hooks.

--- src/components/NavBar/DesktopNavBar/DesktopNavBar.js
+++ src/components/NavBar/DesktopNavBar/DesktopNavBar.js
@@ -1,76 +1,61 @@
-import {Component} from "preact";
+import {useCallback, useEffect, useMemo, useState} from "preact/hooks";
 import classNames from "classnames";
 import {Logo} from "components/common";
 import {debounceWithLeading, scrollTo} from "utils/helpers";
 import "./DesktopNavBar.scss";
 
-export default class DesktopNavBar extends Component {
-    state = {
-        isScrolled: false
-    };
+const NAV_ITEMS = ["about", "skills", "projects", "resume", "contact"];
 
-    componentDidMount() {
-        if (window.location.hash) {
-            this.onNavClick(window.location.hash.split("#")[1])();
+const useScrollToSectionOnLoad = () => {
+    // Only needs to run once on load; other scrolls are handled explicitly.
+    useEffect(() => {
+        const section = window.location.hash;
+
+        if (section) {
+            // Remove the hash before trying to scroll.
+            scrollTo(section.split("#")[1]);
         }
-
-        window.addEventListener("scroll", this.onScroll);
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener("scroll", this.scroll);
-    }
-
-    onNavClick = (id) => () => scrollTo(id);
-
-    onScroll = debounceWithLeading(() => {
-        this.setState({isScrolled: window.pageYOffset > 0});
-    }, 25);
-
-    render() {
-        const {isScrolled} = this.state;
-
-        return (
-            <div className={classNames("navbar-desktop", {"navbar-scrolled": isScrolled})}>
-                <Logo onClick={this.onNavClick("home")} />
-
-                <nav className="navbar-nav">
-                    <a
-                        className="navbar-nav-item"
-                        onClick={this.onNavClick("about")}
-                    >
-                        ABOUT
-                    </a>
-
-                    <a
-                        className="navbar-nav-item"
-                        onClick={this.onNavClick("skills")}
-                    >
-                        SKILLS
-                    </a>
-
-                    <a
-                        className="navbar-nav-item"
-                        onClick={this.onNavClick("projects")}
-                    >
-                        PROJECTS
-                    </a>
-
-                    <a
-                        className="navbar-nav-item"
-                        onClick={this.onNavClick("resume")}
-                    >
-                        RESUME
-                    </a>
-
-                    <a
-                        className="navbar-nav-item"
-                        onClick={this.onNavClick("contact")}
-                    >
-                        CONTACT
-                    </a>
-                </nav>
-            </div>
-        );
-    }
+    }, []);
 };
+
+// Handles transitioning the header from the 'docked' state at the very top of
+// the page (where it doesn't have a drop shadow), to the 'scrolled' state
+// where it does show a drop shadow.
+const useTransitionOnScroll = () => {
+    const [isScrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const onScroll = debounceWithLeading(() => {
+            setScrolled(window.pageYOffset > 0);
+        }, 25);
+
+        window.addEventListener("scroll", onScroll);
+
+        return () => window.removeEventListener("scroll", onScroll);
+    }, [setScrolled]);
+
+    return isScrolled;
+};
+
+const DesktopNavBar = () => {
+    useScrollToSectionOnLoad();
+
+    const isScrolled = useTransitionOnScroll();
+    const onNavClick = useCallback((id) => () => scrollTo(id), []);
+
+    const navItems = useMemo(() => NAV_ITEMS.map((navItem) => (
+        <a className="navbar-nav-item" onClick={onNavClick(navItem)}>{navItem}</a>
+    )), [onNavClick]);
+
+    return (
+        <div className={classNames("navbar-desktop", {"navbar-scrolled": isScrolled})}>
+            <Logo onClick={onNavClick("home")} />
+
+            <nav className="navbar-nav">
+                {navItems}
+            </nav>
+        </div>
+    );
+};
+
+export default DesktopNavBar;
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,7 +1,3 @@
-import {Component} from "preact";
-import classNames from "classnames";
-import {Logo} from "components/common";
-import {debounceWithLeading, scrollTo} from "utils/helpers";
 import DesktopNavBar from "./DesktopNavBar";
 import MobileNavBar from "./MobileNavBar";
 import "./NavBar.scss";
