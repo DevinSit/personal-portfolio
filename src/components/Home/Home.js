3389144dadf47beb4dc6279a54b0ee3009e22d40
Devin Sit <devin@devinsit.com>
2020-04-02 15:09:32 -0400

    Added a little extra emphasis to the tagline; updated packages to fix vulnerabilities.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,7 +6,7 @@ import "./Home.scss";
 const TagLineCircle = () => (
     <div className="tag-line-container">
         <div className="tag-line-circle">
-            <h3 className="tag-line">I solve lots</h3>
+            <h3 className="tag-line">I solve <em>lots</em></h3>
             <h3 className="tag-line">of problems</h3>
             <h1 className="sub-tag-line">Full Spectrum Developer</h1>
         </div>
