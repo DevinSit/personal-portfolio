38157f3c0c133a5881a6047a37f408b789ecefc4
Devin Sit <devin@devinsit.com>
2025-09-26 21:07:58 -0400

    Ported over App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,23 @@
-import {useState} from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import {About, Contact, Footer, Home, NavBar, Projects, Resume, Skills} from "components/";
+import {initAnalytics} from "utils/userAnalytics";
+import "./App.scss";
 
-function App() {
-    const [count, setCount] = useState(0);
+initAnalytics();
 
-    return (
-        <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-        </>
-    );
-}
+const App = () => (
+    <div id="app">
+        <NavBar />
+
+        <div id="app-content">
+            <Home />
+            <About />
+            <Skills />
+            <Projects />
+            <Resume />
+            <Contact />
+            <Footer />
+        </div>
+    </div>
+);
 
 export default App;
